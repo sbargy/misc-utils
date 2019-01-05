0d981f861f20941e73ddee9e6cb7103c6faaa8c0
D. Scott Bargabus <scott@bargabus.com>
2019-01-05 13:27:39 -0800

    First commit, added to README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mac-utils
 Utillities I use on Mac OSX
+
+*   caff.py - calls caffeinate to keep system awake
---
+++ caff.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# system imports
+import argparse
+import sys, os
+
+################################################################################
+def main():
+    parser = argparse.ArgumentParser(description="Call system exe caffeinate \
+            with appropriate args, take hours args and convert to seconds")
+    parser.add_argument("hours",
+                        type = int,
+                        help="Numbers of hours to stay awake",
+                        action="store")
+
+    args = parser.parse_args()
+
+    callCaffeinate(args.hours)
+
+
+################################################################################
+def callCaffeinate(hours):
+    seconds = hours * 3600 # 60 mins * 60 secs to get seconds
+    flags = "-disu" # all the things to set to stay awake
+
+    # Mac OSX specific
+    # assumes caffeinate is installed on the system and is in your path
+    cmd = "caffeinate {} -t {}".format(flags, seconds) # -t is time in seconds
+    print(cmd)
+    os.system(cmd)
+
+################################################################################
+if __name__ == '__main__':
+    main()
